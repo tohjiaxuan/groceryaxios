a1ab1b533df380b230b4b0c063bd4cac4f6ac5bd
tohjiaxuan <61282771+tohjiaxuan@users.noreply.github.com>
2020-03-29 15:08:09 +0800

    update

--- src/SMRT.js
+++ src/SMRT.js
@@ -6,52 +6,82 @@ export default{
     data: () => ({
         results:[],
         chartdata: {
-          //labels:['2020-3-05',4,5,6],
           labels:[],
           datasets: [
             {
-              label: 'MRT, LRT, Bus and Taxi Riders in Singapore',
-               data:[],
-              //backgroundColor:['aqua','lightgreen','red','orange'],
+              label: 'MRT',
+              data:[],
               borderWidth:0.5,
-              borderColor:"grey",
-              backgroundColor:'grey',
+              borderColor:"magenta",
+              backgroundColor:'magenta',
               fill:false
-            }
+            },
+            {
+                label: 'LRT',
+                data:[],
+                borderWidth:0.5,
+                borderColor:"Turquoise",
+                backgroundColor:'turquoise',
+                fill:false
+              },
+              {
+                label: 'Bus',
+                data:[],
+                borderWidth:0.5,
+                borderColor:"red",
+                backgroundColor:'red',
+                fill:false
+              },
+              {
+                label: 'Taxi',
+                data:[],
+                borderWidth:0.5,
+                borderColor: 'blue',
+                backgroundColor: 'blue',
+                fill:false
+              },
           ]
           
         },
         options: {
-           
-            scales:{
-                yAxes:[{
-                    ticks:{
-                        min:0
-                    }
-
-                }]
-            }
+          responsive: true,
+          
         }
       }),
     methods:{
     
     fetchData : function(){
         axios.get('https://data.gov.sg/api/action/datastore_search?resource_id=552b8662-3cbc-48c0-9fbb-abdc07fb377a').then(response=>{
-        // console.log(response.data.result.records)
-        this.results = response.data.result.records
-        for (let key in this.results) {
-            // console.log(this.results[key].average_ridership)
-            this.chartdata.datasets[0].data.push(this.results[key].average_ridership)
-            this.chartdata.labels.push(key+'')
+        this.results=response.data.result.records
+        
+        for(let key in this.results){
+            if (this.results[key].type_of_public_transport == "MRT") {
+                this.chartdata.datasets[0].data.push(this.results[key].average_ridership)
+                this.chartdata.labels.push(this.results[key].year + "")
+            }
+            if (this.results[key].type_of_public_transport  == "LRT") {
+                this.chartdata.datasets[1].data.push(this.results[key].average_ridership)
+            }
+
+            if (this.results[key].type_of_public_transport  == "Bus") {
+                this.chartdata.datasets[2].data.push(this.results[key].average_ridership)
+            }
+
+            if (this.results[key].type_of_public_transport  == "Taxi") {
+                this.chartdata.datasets[3].data.push(this.results[key].average_ridership)
+                
+            }
+            
         }
-        this.renderChart(this.chartdata, this.options)
+        this.renderChart(this.chartdata,this.options)
+            
     })
     
     }
     
     },
      mounted(){
-        //console.log('Do I come here')
+       
         this.fetchData()
         
      }
